4d5ac135fc6d2a2f201f1a79c1b1ab1cd4d1c4a1
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2024-02-07 12:32:40 +0330

    add test to the project

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
+class HomePageTests(TestCase):
+    def test_home_page_url_by_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Home Page')
+
+    def test_home_page_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_template_used(self):
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home.html')
 
 # Create your tests here.
+
+
+class AboutUsPageTests(TestCase):
+
+    def test_about_page_url_by_name(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_content(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertContains(response, 'About Us')
+
+    def test_about_page_template_used(self):
+        response = self.client.get(reverse('aboutus'))
+        self.assertTemplateUsed(response, 'pages/aboutus.html')
