ed7d301b95d9e7b9befe93cd8893555bc9ab828c
mahdibiabani <156238864+mahdibiabani@users.noreply.github.com>
2024-02-09 17:58:59 +0330

    add CommentForm,comment_tags, CustomManager and tabular inline

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from products.views import ProductListView, ProductDetailView
+from products.views import ProductListView, ProductDetailView, CommentCreateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product-list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('comment/<int:product_id>/', CommentCreateView.as_view(), name='comment_create'),
 ]
